95b8ea92066d9ef1c07344bb38b04a1f873f70db
Yevhen Tukubaiev <etukubaev2@gmail.com>
2023-10-01 17:31:59 +0300

    yaml_definitions.mk: add yq_dava function
    Signed-off-by: Yevhen Tukubaiev <etukubaev2@gmail.com>

--- yaml_definitions.mk
+++ yaml_definitions.mk
@@ -8,3 +8,11 @@
 # WARN: while it might not make visual difference, it certainly might
 # break value-based conditions
 _YQ_forcefloat = .00001
+
+# Operator '->' provides easy access to DAVA UI elements
+# Example:
+# .Prototypes[]->b->c   ->   .Prototypes[] | select(."name" == "b").children[] | select(."name" == "c")
+# Usage: $(call yq_dava,.Prototypes[]->b->c)
+define yq_dava =
+$(shell $(ECHO) "$(1)" | $(SED) 's/->/ | select(."name" == "/; s/->/").children[] | select(."name" == "/g; s/$($)/")/')
+endef
